87d26e6eb1450ecc07397e717a1d061c01116712
roger-xavier2 <2995138709@qq.com>
2022-12-28 10:22:04 +0800

    Update SideNav.vue

--- src/components/SideNav/SideNav.vue
+++ src/components/SideNav/SideNav.vue
@@ -25,7 +25,7 @@ aside.global-side-nav(:class='{ hidden: !sideNavStore.isOpen }')
       //-     )
 
       .group
-        .title PixivNow
+        .title 更多年龄向
         ul
           //- list-link(
           //-   icon='external-link-alt',
@@ -35,7 +35,7 @@ aside.global-side-nav(:class='{ hidden: !sideNavStore.isOpen }')
           list-link(
             icon='external-link-alt',
             externalLink='http://pixiv.flexbooru.art/pixiv.apk',
-            text='应用下载&&更多年龄向'
+            text='应用下载'
           )
           list-link(icon='heart', link='/about', text='关于')
 </template>
